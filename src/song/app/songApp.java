adf05f0e71b3b4f29aba43c5ff1613185e41f841
Jack Arteaga <arteagajackCS@gmail.com>
2023-02-13 18:48:05 -0500

    Initial commit

---
+++ src/song/app/songApp.java
@@ -0,0 +1,37 @@
+package song.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import song.view.songController;
+public class songApp extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        // set up FXML loader
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(
+                getClass().getResource  ("/song/view/song.fxml"));
+
+        // load the fxml
+        AnchorPane root = (AnchorPane)loader.load();
+
+        // get the controller (Do NOT create a new Controller()!!)
+        // instead, get it through the loader
+        songController SongController = loader.getController();
+
+        //ListController listController = new ListController();
+
+        SongController.start(primaryStage);
+
+        Scene scene = new Scene(root, 200, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/song/view/songController.java
@@ -0,0 +1,89 @@
+package song.view;
+
+import java.util.Optional;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextInputDialog;
+import javafx.stage.Stage;
+public class songController {
+    @FXML
+    ListView<String> listView;
+
+    private ObservableList<String> obsList;
+
+    public void start(Stage mainStage) {
+        // create an ObservableList
+        // from an ArrayList
+        obsList = FXCollections.observableArrayList(
+                "Rams",
+                "Bengals",
+                "49ers",
+                "Giants",
+                "Packers",
+                "Colts",
+                "Cowboys",
+                "Broncos",
+                "Vikings",
+                "Dolphins",
+                "Titans",
+                "Seahawks",
+                "Steelers",
+                "Jaguars",
+                "Chiefs",
+                "Eagles",
+                "Buccaneers",
+                "Lions");
+
+        listView.setItems(obsList);
+
+        // select the first item
+        listView.getSelectionModel().select(0);
+
+        // set listener for the items
+        listView
+                .getSelectionModel()
+                .selectedIndexProperty()
+                .addListener(
+                        (obs, oldVal, newVal) ->
+                                //showItem(mainStage)
+                                showItemInputDialog(mainStage)
+                );
+    }
+
+    private void showItem(Stage mainStage) {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.initOwner(mainStage);
+        alert.setTitle("List Item");
+        alert.setHeaderText(
+                "Selected list item properties");
+
+        String content = "Index: " +
+                listView.getSelectionModel()
+                        .getSelectedIndex() +
+                "\nValue: " +
+                listView.getSelectionModel()
+                        .getSelectedItem();
+
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    private void showItemInputDialog(Stage mainStage) {
+        String item = listView.getSelectionModel().getSelectedItem();
+        int index = listView.getSelectionModel().getSelectedIndex();
+
+        TextInputDialog dialog = new TextInputDialog(item);
+        dialog.initOwner(mainStage); dialog.setTitle("List Item");
+        dialog.setHeaderText("Selected Item (Index: " + index + ")");
+        dialog.setContentText("Enter name: ");
+
+        Optional<String> result = dialog.showAndWait();
+        if (result.isPresent()) { obsList.set(index, result.get()); }
+    }
+
+}
